5a78e0935492fad5ac3eabde5d8751317507df78
saarthisharma <saarthi.sharma@dianapps.com>
2022-03-11 12:43:42 +0530

    Real time support chat feature implemented

--- routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-// const express = require("express");
-// const app = express();
-// app.use(express.static(__dirname));
-// const router = express.Router();
-
-// router.get("/messages",require("../views/templates/index.html"))
-// module.exports = router
